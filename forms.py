f235f3f0888b1dd1abe63ab5ac69d837a033db1d
Ana Tulea <anatulea14@gmail.com>
2020-12-22 18:02:07 -0800

    Added owner

--- forms.py
+++ forms.py
@@ -3,10 +3,16 @@ from wtforms import StringField, IntegerField, SubmitField
 
 class AddForm(FlaskForm):
 
-    name = StringField('Name of Puppy: ')
+    name = StringField('Name of Puppy:')
     submit = SubmitField('Add Puppy')
 
+class AddOwnerForm(FlaskForm):
+
+    name = StringField('Name of Owner:')
+    pup_id = IntegerField("Id of Puppy: ")
+    submit = SubmitField('Add Owner')
+
 class DelForm(FlaskForm):
-    
-    id = IntegerField('Id Number of Puppy to Remove: ')
+
+    id = IntegerField('Id Number of Puppy to Remove:')
     submit = SubmitField('Remove Puppy')
